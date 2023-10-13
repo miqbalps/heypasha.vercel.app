04912ab498898eef5541a93a46fe74e6988d2cfe
Iqbal Pasha <iqbalpasha6a@gmail.com>
2023-10-13 13:06:26 +0700

    remove navbar and footer

--- app/layout.js
+++ app/layout.js
@@ -19,9 +19,9 @@ export default function RootLayout({ children }) {
       >
         <div className="bg-[#fbe2e3] -z-50 absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.2rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-[#dbd7fb] -z-50 absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[31.2rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
-        <NavbarSimple />
+        {/* <NavbarSimple /> */}
         {children}
-        <SimpleFooter />
+        {/* <SimpleFooter /> */}
       </body>
     </html>
   );
