ac8b6621e9f55e94627d1f21a6ff3cf3f1b5eb33
Iqbal Pasha <iqbalpasha6a@gmail.com>
2023-10-15 15:17:57 +0700

    p

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,7 @@
 import "./globals.css";
-import React from "react";
 import { Inter } from "next/font/google";
-import {
-  Navbar,
-  Collapse,
-  Typography,
-  IconButton,
-  Bars3Icon,
-  XMarkIcon,
-} from "@/components/MaterialTailwind";
+import { Typography } from "@/components/MaterialTailwind";
+import { NavbarSimple } from "@/components/NavbarComponent";
 import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -35,101 +28,7 @@ export default function RootLayout({ children }) {
   );
 }
 
-function NavList() {
-  return (
-    <ul className="my-2 flex flex-col gap-2 md:mb-0 md:mt-0 md:flex-row md:items-center md:gap-6">
-      <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-1 font-bold text-md"
-      >
-        <Link
-          href="/"
-          className="flex items-center hover:text-teal-500 transition-colors"
-        >
-          Home
-        </Link>
-      </Typography>
-      <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-1 font-bold text-md"
-      >
-        <Link
-          href="/about"
-          className="flex items-center hover:text-teal-500 transition-colors"
-        >
-          About
-        </Link>
-      </Typography>
-      <Typography
-        as="li"
-        variant="small"
-        color="blue-gray"
-        className="p-1 font-bold text-md"
-      >
-        <Link
-          href="/projects"
-          className="flex items-center hover:text-teal-500 transition-colors"
-        >
-          Projects
-        </Link>
-      </Typography>
-    </ul>
-  );
-}
-
-function NavbarSimple() {
-  const [openNav, setOpenNav] = React.useState(false);
-
-  const handleWindowResize = () =>
-    window.innerWidth >= 960 && setOpenNav(false);
-
-  React.useEffect(() => {
-    window.addEventListener("resize", handleWindowResize);
-
-    return () => {
-      window.removeEventListener("resize", handleWindowResize);
-    };
-  }, []);
-
-  return (
-    <Navbar className="mx-auto max-w-screen-lg px-6 my-3 z-10 relative">
-      <div className="flex items-center justify-between text-blue-gray-900">
-        <Typography
-          as="a"
-          href="/"
-          variant="h6"
-          className="mr-4 cursor-pointer py-1.5 text-xl"
-        >
-          Mikuah
-        </Typography>
-        <div className="hidden md:block">
-          <NavList />
-        </div>
-        <IconButton
-          variant="text"
-          className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent md:hidden"
-          ripple={false}
-          onClick={() => setOpenNav(!openNav)}
-        >
-          {openNav ? (
-            <XMarkIcon className="h-6 w-6" strokeWidth={2} />
-          ) : (
-            <Bars3Icon className="h-6 w-6" strokeWidth={2} />
-          )}
-        </IconButton>
-      </div>
-      <Collapse open={openNav}>
-        <NavList />
-      </Collapse>
-    </Navbar>
-  );
-}
-
-export function SimpleFooter() {
+function SimpleFooter() {
   return (
     <footer className="flex w-full flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 border-t border-blue-gray-50 py-6 text-center md:justify-between max-w-screen-lg px-6 mx-auto">
       <Typography color="blue-gray" className="font-normal">
