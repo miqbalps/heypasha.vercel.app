1c83ab9691f2a06374870003fa57e802c37a659d
miqbalps <iqbalpasha6a@gmail.com>
2024-10-05 23:31:12 +0700

    minor

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,8 +17,8 @@ export default function RootLayout({ children }) {
       <body
         className={`${inter.className} min-h-screen flex flex-col bg-gray-50 text-gray-950`}
       >
-        <div className="bg-[#fbe2e3] -z-50 absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.2rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-        <div className="bg-[#dbd7fb] -z-50 absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[31.2rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        {/* <div className="bg-[#fbe2e3] -z-50 absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.2rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+        <div className="bg-[#dbd7fb] -z-50 absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[31.2rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div> */}
         <NavbarSimple />
         {children}
         <SimpleFooter />
